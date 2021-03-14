cae12c2bb78dbd7e7f532ab187b982c126dbeb3d
Alex-Lin-Fei <2055017970@qq.com>
2021-03-14 22:16:22 +0800

    add fragment_member_list.xml

--- AssassinsList/app/src/main/java/com/example/assassinslist/AssassinsListFragment.kt
+++ AssassinsList/app/src/main/java/com/example/assassinslist/AssassinsListFragment.kt
@@ -2,12 +2,20 @@ package com.example.assassinslist
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlin.math.log
 
 private const val TAG = "AssassinsListFragment"
 class AssassinsListFragment: Fragment() {
+
+    private lateinit var assassinRecyclerView: RecyclerView
+
     private val assassinsListViewModel: AssassinsListViewModel by lazy {
         ViewModelProvider(this)[AssassinsListViewModel::class.java]
     }
@@ -15,6 +23,17 @@ class AssassinsListFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total members: ${assassinsListViewModel.members.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_member_list, container, false)
+        assassinRecyclerView = view.findViewById(R.id.member_recycler_view) as RecyclerView
+        assassinRecyclerView.layoutManager = LinearLayoutManager(context)
+        return view
     }
 
     companion object {
