6ff671e914a61139a6df7104f8825662d7b67ae6
Alex-Lin-Fei <2055017970@qq.com>
2021-03-15 15:51:42 +0800

    add MemberDao

--- AssassinsList/app/src/main/java/com/example/assassinslist/dabatase/MemberDao.kt
+++ AssassinsList/app/src/main/java/com/example/assassinslist/dabatase/MemberDao.kt
@@ -1,4 +1,16 @@
 package com.example.assassinslist.dabatase
 
-class MemberDao {
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Query
+import com.example.assassinslist.Member
+import java.util.*
+
+@Dao
+interface MemberDao {
+    @Query("SELECT * FROM Member")
+    fun getMembers(): List<Member>
+
+    @Query("SELECT * FROM Member WHERE id = (:id)")
+    fun getMember(id: UUID): Member?
 }
--- AssassinsList/app/src/main/java/com/example/assassinslist/dabatase/MemberDatabase.kt
+++ AssassinsList/app/src/main/java/com/example/assassinslist/dabatase/MemberDatabase.kt
@@ -9,5 +9,5 @@ import com.example.assassinslist.Member
 @Database(entities = [Member::class], version = 1)
 @TypeConverters(MemberTypeConverters::class)
 abstract class MemberDatabase: RoomDatabase() {
-
+    abstract fun memberDao(): MemberDao
 }
