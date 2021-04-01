6cdbe2f6b2d0fee0bfbf6afc015216e40e2318d4
Alex-Lin-Fei <2055017970@qq.com>
2021-04-01 13:53:55 +0800

    add InfoActivity

--- MyUITest/app/src/main/java/com/example/myuitest/InfoActivity.kt
+++ MyUITest/app/src/main/java/com/example/myuitest/InfoActivity.kt
@@ -1,11 +1,42 @@
 package com.example.myuitest
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
+private const val EXTRA_NAME = "name"
+private const val EXTRA_AGE = "age"
+private const val EXTRA_HEIGHT = "height"
 class InfoActivity : AppCompatActivity() {
+
+    private lateinit var name: String
+    private lateinit var age: String
+    private lateinit var height: String
+
+    private lateinit var InfoTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        InfoTextView = findViewById(R.id.info)
+
+        name = intent.getStringExtra(EXTRA_NAME).toString()
+        age = intent.getStringExtra(EXTRA_AGE).toString()
+        height = intent.getStringExtra(EXTRA_HEIGHT).toString()
+
+        InfoTextView.text = getString(R.string.info, name, age, height)
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, name: String, age: String, height: String): Intent {
+            return Intent(packageContext, InfoActivity::class.java).apply {
+                putExtra(EXTRA_NAME, name)
+                putExtra(EXTRA_AGE, age)
+                putExtra(EXTRA_HEIGHT, height)
+            }
+        }
     }
 }
--- MyUITest/app/src/main/java/com/example/myuitest/MainActivity.kt
+++ MyUITest/app/src/main/java/com/example/myuitest/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.myuitest
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ActionMenuView
 import android.widget.Button
 import android.widget.EditText
+
+private const val SHOW_INFO = 1
 
 class MainActivity : AppCompatActivity() {
     private lateinit var nameEditText: EditText
@@ -27,6 +33,26 @@ class MainActivity : AppCompatActivity() {
             nameEditText.setText("")
             ageEditText.setText("")
             heightEditText.setText("")
+        }
+
+        showInfo.setOnClickListener {
+            val name = nameEditText.text.toString()
+            val age = ageEditText.text.toString()
+            val height = heightEditText.text.toString()
+            val intent = InfoActivity.newIntent(this@MainActivity, name, age, height)
+            startActivityForResult(intent, SHOW_INFO)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        if (requestCode == SHOW_INFO) {
+
         }
     }
 }
