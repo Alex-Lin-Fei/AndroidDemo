0f5e107e1960893883d5454a4e8a243fe18ad1ab
Alex-Lin-Fei <2055017970@qq.com>
2021-04-01 14:35:13 +0800

    add return button

--- MyUITest/app/src/main/java/com/example/myuitest/InfoActivity.kt
+++ MyUITest/app/src/main/java/com/example/myuitest/InfoActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myuitest
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 private const val EXTRA_NAME = "name"
@@ -16,19 +18,26 @@ class InfoActivity : AppCompatActivity() {
     private lateinit var height: String
 
     private lateinit var InfoTextView: TextView
+    private lateinit var returnButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
         InfoTextView = findViewById(R.id.info)
+        returnButton = findViewById(R.id.return_button)
 
         name = intent.getStringExtra(EXTRA_NAME).toString()
         age = intent.getStringExtra(EXTRA_AGE).toString()
         height = intent.getStringExtra(EXTRA_HEIGHT).toString()
 
         InfoTextView.text = getString(R.string.info, name, age, height)
+        returnButton.setOnClickListener {
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
     }
+
 
     companion object {
         fun newIntent(packageContext: Context, name: String, age: String, height: String): Intent {
