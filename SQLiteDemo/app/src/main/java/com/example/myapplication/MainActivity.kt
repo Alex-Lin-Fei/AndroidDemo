8707c14a98c3625f72ae4943a25ad38a743a85e2
Alex-Lin-Fei <2055017970@qq.com>
2021-04-14 13:25:13 +0800

    create database book

--- SQLiteDemo/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ SQLiteDemo/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var createDatabase: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createDatabase = findViewById(R.id.createDatabase)
+
+        val dbHelper = DatabaseHelper(this, "BookStore.db", 1)
+        createDatabase.setOnClickListener {
+            dbHelper.writableDatabase
+        }
     }
+
 }
