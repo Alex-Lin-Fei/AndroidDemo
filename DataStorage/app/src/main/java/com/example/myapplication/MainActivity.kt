29ad4e2d6c9c74a58a129b6562a31186f9db8a99
Alex-Lin-Fei <2055017970@qq.com>
2021-04-14 12:43:50 +0800

    sharedpreferences and file

--- DataStorage/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ DataStorage/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,90 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.io.*
+import java.lang.StringBuilder
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    private lateinit var editText: EditText
+    private lateinit var saveButton: Button
+    private lateinit var restoreButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        saveButton = findViewById(R.id.saveButton)
+        restoreButton = findViewById(R.id.restoreButton)
+
+        val inputText = load()
+        if (inputText.isNotEmpty()) {
+            editText.setText(inputText)
+            editText.setSelection(inputText.length)
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show()
+        }
+
+        saveButton.setOnClickListener {
+            val editor = getSharedPreferences("data",
+            Context.MODE_PRIVATE).edit()
+            editor.putString("name", "Tom")
+            editor.putInt("age", 20)
+            editor.putBoolean("married", false)
+            editor.apply()
+        }
+
+
+        restoreButton.setOnClickListener {
+            val prefs = getSharedPreferences("data", Context.MODE_PRIVATE)
+            val name = prefs.getString("name", "")
+            val age = prefs.getInt("age", 0)
+            val married = prefs.getBoolean("married", false)
+            Log.d(TAG, "name is $name")
+            Log.d(TAG, "age is $age")
+            Log.d(TAG, "married is $married")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val inputText = editText.text.toString()
+        save(inputText)
+    }
+
+    private fun save(inputText: String) {
+        try {
+            val output = openFileOutput(
+                "data", Context.MODE_PRIVATE)
+            val writer = BufferedWriter(OutputStreamWriter(output))
+            writer.use {
+                it.write(inputText)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun load(): String {
+        val content = StringBuilder()
+        try {
+            val input = openFileInput("data")
+            val reader = BufferedReader(InputStreamReader(input))
+            reader.useLines {
+                reader.forEachLine {
+                    content.append(it)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return content.toString()
     }
 }
