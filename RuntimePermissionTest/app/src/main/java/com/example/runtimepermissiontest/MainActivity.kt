2e95445b3227ad3863c86d82b1956558d323d6c8
Alex-Lin-Fei <2055017970@qq.com>
2021-04-28 19:47:08 +0800

    manifest

--- RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.kt
+++ RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.runtimepermissiontest
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
@@ -26,7 +29,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun call() {
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
 }
