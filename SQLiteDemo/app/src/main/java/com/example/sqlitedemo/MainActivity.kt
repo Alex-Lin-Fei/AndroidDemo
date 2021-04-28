b9bd01c12292c3bd5c3762708cd4f8a75496e2ea
Alex-Lin-Fei <2055017970@qq.com>
2021-04-28 15:52:15 +0800

    add oncreate and onupdate

--- SQLiteDemo/app/src/main/java/com/example/sqlitedemo/MainActivity.kt
+++ SQLiteDemo/app/src/main/java/com/example/sqlitedemo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.sqlitedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var createDatabase: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dbHelper = MyDatabaseHelper(this, "BookStore.db", 2)
+        createDatabase = findViewById(R.id.createDb)
+        createDatabase.setOnClickListener {
+            dbHelper.writableDatabase
+        }
     }
 }
