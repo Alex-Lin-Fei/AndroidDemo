00395d75af3c5bbc07a111a3e3eeaeabe872f022
Alex-Lin-Fei <2055017970@qq.com>
2021-05-08 21:00:29 +0800

    start to revise

---
+++ Lab/DataAccess/app/src/main/java/com/example/dataaccess/MyService.kt
@@ -0,0 +1,34 @@
+package com.example.dataaccess
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlin.concurrent.thread
+
+private const val TAG = "MyService"
+class MyService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate executed")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(TAG, "onStartCommand executed")
+        thread {
+
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy executed")
+    }
+}
+
