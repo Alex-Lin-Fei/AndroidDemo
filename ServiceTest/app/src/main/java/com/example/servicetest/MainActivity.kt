8c5335f414982339cee37728e853b575b7d2226f
Alex-Lin-Fei <2055017970@qq.com>
2021-05-19 10:07:11 +0800

    add something

--- ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.kt
+++ ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.servicetest
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var downloadBinder: MyService.DownloadBinder
+
+    private val connection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            downloadBinder = service as MyService.DownloadBinder
+            downloadBinder.startDownload()
+            downloadBinder.getProgress()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+    }
+
+    private lateinit var startServiceBtn: Button
+    private lateinit var stopServiceBtn: Button
+    private lateinit var bindServiceBtn: Button
+    private lateinit var unbindServiceBtn: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startServiceBtn = findViewById(R.id.startServiceBtn)
+        stopServiceBtn = findViewById(R.id.stopServiceBtn)
+        bindServiceBtn = findViewById(R.id.bindServiceBtn)
+        unbindServiceBtn = findViewById(R.id.unbindServiceBtn)
+
+
+        startServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            startService(intent) // 启动服务
+        }
+
+        stopServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            stopService(intent) // 停止service
+        }
+
+        bindServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+
     }
 }
--- ServiceTest/app/src/main/java/com/example/servicetest/MyService.kt
+++ ServiceTest/app/src/main/java/com/example/servicetest/MyService.kt
@@ -2,11 +2,43 @@ package com.example.servicetest
 
 import android.app.Service
 import android.content.Intent
+import android.nfc.Tag
+import android.os.Binder
 import android.os.IBinder
+import android.util.Log
 
+private const val TAG = "MyService"
 class MyService : Service() {
 
+    private val mBinder = DownloadBinder()
+
+    class DownloadBinder: Binder() {
+        fun startDownload() {
+            Log.d(TAG, "startDownload executed")
+        }
+
+        fun getProgress(): Int {
+            Log.d(TAG, "getProgress executed")
+            return 0
+        }
+    }
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return mBinder
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate executed")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(TAG, "onStartCommand executed")
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy executed")
     }
 }
